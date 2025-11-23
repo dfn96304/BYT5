926eee9a8281f847811234bc197da34f6794cd80
dfn96304 <s30496@pjwstk.edu.pl>
2025-11-23 14:06:58 +0100

    Add exception handling wrapper (rethrow for this example)

--- DesignPattern/Adapter/EmployeeAdapter.cs
+++ DesignPattern/Adapter/EmployeeAdapter.cs
@@ -10,13 +10,20 @@ namespace DesignPattern.Adapter
             List<Employee> employeesList = new();
             for (int i = 0; i < employeesArray.GetLength(0); i++)
             {
-                // error handling?
-                int ID = Convert.ToInt32(employeesArray[i, 0]);
-                string name = employeesArray[i, 1];
-                string designation = employeesArray[i, 2];
-                decimal salary = Convert.ToDecimal(employeesArray[i, 3]);
-
-                employeesList.Add(new Employee(ID, name, designation, salary));
+                try
+                {
+                    int ID = Convert.ToInt32(employeesArray[i, 0]);
+                    string name = employeesArray[i, 1];
+                    string designation = employeesArray[i, 2];
+                    decimal salary = Convert.ToDecimal(employeesArray[i, 3]);
+                    
+                    employeesList.Add(new Employee(ID, name, designation, salary));
+                }
+                catch (Exception ex)
+                {
+                    Console.Error.WriteLine("EmployeeAdapter: exception while processing");
+                    throw ex;
+                }
             }
             thirdPartyBillingSystem.ProcessSalary(employeesList);
         }
