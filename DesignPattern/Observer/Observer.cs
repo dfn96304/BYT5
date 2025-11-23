d8be9829508c4ee126606c5b46a0727d95d9c2bd
dfn96304 <s30496@pjwstk.edu.pl>
2025-11-23 16:11:30 +0100

    Implement Observer

--- DesignPattern/Observer/Observer.cs
+++ DesignPattern/Observer/Observer.cs
@@ -10,5 +10,22 @@ namespace DesignPattern.Observer
         //3. Registering the Observer with the Subject
         //4. Removing the Observer from the Subject
         //5. Observer will get a notification from the Subject using the following Method
+        
+        public string UserName { get; set; } = userName; // 1
+        
+        public void AddSubscriber(ISubject subject) // 3
+        {
+            subject.RegisterObserver(this);
+        }
+
+        public void RemoveSubscriber(ISubject subject) // 4
+        {
+            subject.RemoveObserver(this);
+        }
+
+        public void Update(string availability)
+        {
+            
+        }
     }
 }
