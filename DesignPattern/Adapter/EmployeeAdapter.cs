1be68a08b64520d21aae4fc63624bd23afb1e1b6
dfn96304 <s30496@pjwstk.edu.pl>
2025-11-23 14:02:16 +0100

    Initial commit + part of adapter pattern

---
+++ DesignPattern/Adapter/EmployeeAdapter.cs
@@ -0,0 +1,24 @@
+﻿// In case you need some guidance: https://refactoring.guru/design-patterns/adapter
+namespace DesignPattern.Adapter
+{
+    public class EmployeeAdapter : ITarget
+    {
+        private readonly BillingSystem thirdPartyBillingSystem = new();
+
+        public void ProcessCompanySalary(string[,] employeesArray)
+        {
+            List<Employee> employeesList = new();
+            for (int i = 0; i < employeesArray.GetLength(0); i++)
+            {
+                // error handling?
+                int ID = Convert.ToInt32(employeesArray[i, 0]);
+                string name = employeesArray[i, 1];
+                string designation = employeesArray[i, 2];
+                decimal salary = Convert.ToDecimal(employeesArray[i, 3]);
+
+                employeesList.Add(new Employee(ID, name, designation, salary));
+            }
+            thirdPartyBillingSystem.ProcessSalary(employeesList);
+        }
+    }
+}
